fd369acd02ea7b6948e2113f0b81947022a1bc19
Ishita Thapliyal <56544990+itta10@users.noreply.github.com>
2022-03-30 10:10:24 +0530

    Add files via upload

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+
+  let firebaseConfig = {
+    apiKey: "AIzaSyA1EeocbJihxn7r12pGb0iDYVH2Vs2t-Qc",
+    authDomain: "blogging-website-2084c.firebaseapp.com",
+    projectId: "blogging-website-2084c",
+    storageBucket: "blogging-website-2084c.appspot.com",
+    messagingSenderId: "487561282679",
+    appId: "1:487561282679:web:9512e3a3db207297d3b272"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+   let db=firebase.firestore();
